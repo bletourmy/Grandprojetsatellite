2f70b731c0d15595c576cf89ffb82f33d2af33d9
Bernard Letourmy <bernard.letourmy@apilinx.com>
2015-11-04 22:50:28 +0800

    autorise l import des fonctions obtenir_azimut_et_hauteur
    - afin d'eviter que le programme principal du module s'execute lors de
    l'import de fonctions par un autre module.
    Nous deplacons le code global (qui s'executait en chargeant le module
    depuis python) dans une fonction que l'on appelle ici main() (par convention)
    
    puis on appelle cette fonction main "automatiquement" via
    if __name__ == "__main__":
        main()
    uniquement quand le module est chargee en tant que module principal
    c.a.d quand il est lance par:
    python obtenir_azimut_et_hauteur.py
    
    par contre quand le module sera charge par ex. via un:
    
    from obtenir_aimut_et_hauteur.py import equation_celestes
    
    __name__ sera != "__main__" et main() ne sera donc pas executee

--- obtenir_azimut_et_hauteur.py
+++ obtenir_azimut_et_hauteur.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8-*-
+from __future__ import print_function
+
 from math import *
+
 def spher_to_cart(r,theta,phi):
     """
 converti coordonnees spheriques en cartesiens, les angles sont en degres
@@ -27,48 +32,50 @@ soustrait le vecteur w a v, ce sont des dictionnaires contenant x,y,z
 
     
 
+def main():
+
+    r_s=float(input("entrez la l'altitude du satellite "))
+    theta_s=float(input("entrez la longitude du satellite "))
+    phi_s=float(input("entrez la lattitude du satellite "))
+    r_o=float(input("entrez la l'altitude de l'observateur "))
+    theta_o=float(input("entrez la longitude de l'observateur "))
+    phi_o=float(input("entrez la lattitude de l'observateur "))
+
+
+    coord_sat = spher_to_cart(r_s, theta_s, phi_s)
+
+
+    coord_obs = spher_to_cart(r_o, theta_o, phi_o)
+
+    coord_relat_sat = sub_vect(coord_sat, coord_obs)
+
+    #cart_to_spher(coord_relat_sat['x'], coord_relat_sat['y'], coord_relat_sat['z'])
+    coord_celestes= cart_to_spher(**coord_relat_sat)  #c'est un raccourci, ca disloque le contenu du dictionnaire
+
+    print ("les coordonnees celestes sont :", coord_celestes) #c'est pas fini, afficher un beau resultat intermediaire
+    print("Lest coord celestes sont : Déclinaison:{} asdasd:{}".format(coord_celestes['theta'],coord_celestes['phi']))
+
+    def equations_celestes(lat_obs, phi, theta):
+        phi = radians(phi)
+        theta = radians(theta)
+        lat_obs = radians(lat_obs) 
+        h = asin(cos(lat_obs))*cos(phi)*cos(theta)+sin(lat_obs)*sin(phi)
+        z = acos((sin(lat_obs)*cos(phi)*cos(theta)-cos(lat_obs)*sin(phi))/cosh(h))
+        cos_h= cos(phi)*sin(theta)/sin(z)
+        sin_z= cos(phi)* sin(theta)/ cos(h)
+        if cos_h<0:
+            h=-h+pi
+            if sin_z < 0:
+                z=-z
+                return {'h':degrees(h), 'z':degrees(z)}
+
+
+    azimut_et_hauteur = equations_celestes(phi_o, coord_celestes['phi'], coord_celestes['theta'])
+
+    print ( azimut_et_hauteur )
+    #delta: phi(lat) Ah:theta(long)
+
+if __name__ == "__main__":
+    main()
     
-r_s=float(input("entrez la l'altitude du satellite "))
-theta_s=float(input("entrez la longitude du satellite "))
-phi_s=float(input("entrez la lattitude du satellite "))
-r_o=float(input("entrez la l'altitude de l'observateur "))
-theta_o=float(input("entrez la longitude de l'observateur "))
-phi_o=float(input("entrez la lattitude de l'observateur "))
-
-
-coord_sat = spher_to_cart(r_s, theta_s, phi_s)
-
-
-coord_obs = spher_to_cart(r_o, theta_o, phi_o)
-
-coord_relat_sat = sub_vect(coord_sat, coord_obs)
-
-#cart_to_spher(coord_relat_sat['x'], coord_relat_sat['y'], coord_relat_sat['z'])
-coord_celestes= cart_to_spher(**coord_relat_sat)  #c'est un raccourci, ca disloque le contenu du dictionnaire
-
-print ("les coordonnees celestes sont :", coord_celestes) #c'est pas fini, afficher un beau resultat intermediaire
-print("Lest coord celestes sont : Déclinaison:{} asdasd:{}".format(coord_celestes['theta'],coord_celestes['phi']))
-
-def equations_celestes(lat_obs, phi, theta):
-    phi = radians(phi)
-    theta = radians(theta)
-    lat_obs = radians(lat_obs) 
-    h = asin(cos(lat_obs))*cos(phi)*cos(theta)+sin(lat_obs)*sin(phi)
-    z = acos((sin(lat_obs)*cos(phi)*cos(theta)-cos(lat_obs)*sin(phi))/cosh(h))
-    cos_h= cos(phi)*sin(theta)/sin(z)
-    sin_z= cos(phi)* sin(theta)/ cos(h)
-    if cos_h<0:
-        h=-h+pi
-    if sin_z < 0:
-        z=-z
-    return {'h':degrees(h), 'z':degrees(z)}
-
-
-azimut_et_hauteur = equations_celestes(phi_o, coord_celestes['phi'], coord_celestes['theta'])
-
-print ( azimut_et_hauteur )
-    
-
-#delta: phi(lat) Ah:theta(long)
-
 
